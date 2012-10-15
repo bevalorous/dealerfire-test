5f8a25de7c09e0a39f7cc7013fbc9476fa989724
Vadim Belorussov <belorussov.vadim@gmail.com>
2012-10-15 10:02:36 +0500

    Add method renderPartial() to base class Controller

--- base/Controller.php
+++ base/Controller.php
@@ -10,4 +10,14 @@ class Controller
         $className = get_class($this);
         require('protected/views/'.substr($className, 0, strripos($className, 'controller')).'/'.$view.'.php');
     }
+	
+	public function renderPartial($view, $data)
+    {
+        foreach ($data as $name => $value)
+        {
+            $$name = $value;
+        }        
+        $className = get_class($this);
+        require('protected/views/'.substr($className, 0, strripos($className, 'controller')).'/'.$view.'.php');
+    }
 }
